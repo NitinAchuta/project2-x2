a4e0d2b750ced680b0d8bdf895c99adb4979d7d8
NitinAchuta <goodlucknitin@gmail.com>
2025-09-26 10:53:04 -0500

    sql files to create the table

---
+++ customers.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Customers (
+	customer_ID INT PRIMARY KEY,
+	name VARCHAR(100),
+	phone_number VARCHAR(20),
+	email VARCHAR(100)
+);
---
+++ employees.sql
@@ -0,0 +1,6 @@
+CREATE TABLE employees (
+	employee_ID INT PRIMARY KEY,
+	name VARCHAR(100),
+	role VARCHAR(100),
+	schedule DATE
+);
