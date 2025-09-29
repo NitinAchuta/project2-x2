790048431a43609f77b29314e9a38bd1e7232f89
hariko23 <harryko@tamu.edu>
2025-09-29 14:41:43 -0500

    fixed first three special queries

--- queries.sql
+++ queries.sql
@@ -1,9 +1,9 @@
 -- 1. count of orders grouped by week
-SELECT COUNT(*) FROM orders GROUP BY week;
+SELECT orderWeek, COUNT(*) FROM orders GROUP BY orderWeek;
 
 -- 2. count of orders grouped by hour
 SELECT
-  EXTRACT(HOUR FROM "datetime") AS hour_of_day,
+  EXTRACT(HOUR FROM "timeoforder") AS hour_of_day,
   COUNT(*)
 FROM orders
 GROUP BY hour_of_day
@@ -11,11 +11,11 @@ ORDER BY hour_of_day;
 
 -- 3. peak sales day
 SELECT
-    EXTRACT(DAY FROM "datetime") AS _date,
-    SUM(total_amount) AS total
+    "timeoforder"::date AS order_date,
+    SUM(totalcost) AS total
 FROM orders 
-GROUP BY _date
-ORDER BY _date
+GROUP BY order_date
+ORDER BY order_date
 LIMIT 10;
 
 --4. Menu Item Inventory
