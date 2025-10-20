4ba742f6e7c06c27ea09954b96fcb7e4181c2765
tdocog <thomasdocog@gmail.com>
2025-10-20 16:24:22 -0500

    starting the refactoring of the BobaShopManagerGUI.java file

---
+++ ManagerGUI/ManagerUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+
+/**
+ * ManagerUI provides the interface for managers to handle operations
+ * Modular design with improved organization and cleaner UI
+ */
+public class ManagerUI extends JFrame {
+    
+}
