7cc6f0c1098bab3d7ae8c5879f6ff88ed9db2049
Harry <koharrisonko@gmail.com>
2025-10-20 14:23:55 -0500

    Created attributes for mock data

--- New_Additions/MockDataProvider.java
+++ New_Additions/MockDataProvider.java
@@ -1,5 +1,18 @@
 package New_Additions;
 
+import java.sql.Timestamp;
+import java.util.*;
+
 public class MockDataProvider {
-    
+    private List<MenuItem> menuItems;
+    private List<Inventory> inventory;
+    private List<Employee> employees;
+    private List<Order> orders;
+    private List<OrderItem> orderItems;
+
+    private int nextMenuItemId = 1;
+    private int nextInventoryId = 1;
+    private int nextEmployeeId = 1;
+    private int nextOrderId = 1;
+    private int nextOrderItemId = 1;
 }
